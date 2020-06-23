55451c408b40b468da1524defdda81fb6049608c
demonatic <793626853@qq.com>
2020-06-23 19:43:04 +0800

    grid layout adaptive height

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,52 +4,57 @@
           <el-button v-if='header_show_flag' class="go-top" @click="scroll_to_top" circle>
             <i class="fa fa-angle-up"></i>
           </el-button>
-          <div v-bind:class="header_show_flag?'fixed-search':'float-search'">
-            <header class="top-header" v-if='header_show_flag'>
-              <div class="header-menu" id="find-menu" @click="do_recommendation()">
-                <i class="fa fa-th"></i>
-                <span>发现</span>
-                <transition name='underline-fade'>
-                  <div v-if='recommend_search_toggle' class='underline'></div>
-                </transition>
+          <div class="on-background-content">
+            <div v-bind:class="header_show_flag?'fixed-search':'float-search'">
+              <header class="top-header" v-if='header_show_flag'>
+                <div class="header-menu-group">
+                  <div class="header-menu" id="find-menu" @click="do_recommendation()">
+                    <i class="fa fa-th"></i>
+                    <span>发现</span>
+                    <transition name='underline-fade'>
+                      <div v-if='recommend_search_toggle' class='underline'></div>
+                    </transition>
+                  </div>
+                  <div class="header-menu" id="search-menu" @click="do_search()">
+                    <i class="fa fa-dot-circle-o"></i>
+                    <span>搜索</span>
+                    <transition name='underline-fade'>
+                      <div v-if='!recommend_search_toggle' class='underline'></div>
+                    </transition>
+                  </div>
+                </div>
+              </header>
+              <div class="search-input">
+                <i class="fa fa-search" @click="do_search()"></i>
+                <input type="text" maxlength="46" v-model="keyword" placeholder="Search recipes"
+                  @keyup="get_suggestion($event)" @keydown.enter="do_search()" @keydown.down="suggestion_down()" @keydown.up.prevent="suggestion_up()"
+                  @focus="show_suggestion()" @blur="hide_suggestion()"
+                >
+                <span class="search-clear" @click="clear_input()">&times;</span>
+                <div class="search-select" v-if="suggestion_show_flag">
+                  <transition-group name="itemfade" tag="ul" mode="out-in" v-cloak>
+                    <li v-for="(value,index) in suggest_items" :class="{selectback:index==current_index}" class="search-select-option search-select-list"
+                      @mouseover="suggestion_hover(index)" @click="suggestion_click(index)" :key="value">
+                      {{value}}
+                    </li>
+                  </transition-group>
+                </div>
+                <div class="search-description">
+                  <span id="search-description-left" v-if='!header_show_flag'>食谱／食材</span>
+                  <span id="search-description-right" v-if='!header_show_flag'>中餐／西餐／面食／甜点／饮料</span>
+                </div>
               </div>
-              <div class="header-menu" id="search-menu" @click="do_search()">
-                <i class="fa fa-dot-circle-o"></i>
-                <span>搜索</span>
-                <transition name='underline-fade'>
-                  <div v-if='!recommend_search_toggle' class='underline'></div>
-                </transition>
-              </div>
-            </header>
-            <div class="search-input">
-              <i class="fa fa-search" @click="do_search()"></i>
-              <input type="text" maxlength="46" v-model="keyword" placeholder="Search recipes"
-                @keyup="get_suggestion($event)" @keydown.enter="do_search()" @keydown.down="suggestion_down()" @keydown.up.prevent="suggestion_up()"
-                @focus="show_suggestion()" @blur="hide_suggestion()"
-              >
-              <span class="search-clear" @click="clear_input()">&times;</span>
-              <div class="search-select" v-if="suggestion_show_flag">
-                <transition-group name="itemfade" tag="ul" mode="out-in" v-cloak>
-                  <li v-for="(value,index) in suggest_items" :class="{selectback:index==current_index}" class="search-select-option search-select-list"
-                    @mouseover="suggestion_hover(index)" @click="suggestion_click(index)" :key="value">
-                    {{value}}
-                  </li>
-                </transition-group>
-              </div>
-            </div>
-            <div class="search-description">
-              <span>食谱／食材 &emsp;&emsp;&emsp;&emsp;中餐／西餐／面食／甜点／饮料</span>
-            </div>
-            <div class="navigator" @click="scroll_to_search_result()">
-              <span v-if="recommend_search_toggle">为你推荐</span>
-              <span v-if="!recommend_search_toggle">'{{searched_keyword}}' &nbsp;的搜索结果</span>
-              <i v-if="!is_loading" class="el-icon-d-arrow-right" style="transform: rotate(90deg); font-size: xx-large"></i>
-              <i v-if="is_loading" class="el-icon-loading" style="font-size: xx-large"></i>
             </div>
           </div>
         </div> <!-- background-image -->
+        <div class="navigator" @click="scroll_to_search_result()">
+            <span v-if="recommend_search_toggle">为你推荐</span>
+            <span v-if="!recommend_search_toggle">'{{searched_keyword}}' &nbsp;的搜索结果</span>
+            <i v-if="!is_loading" class="el-icon-d-arrow-right" style="transform: rotate(90deg); font-size: xx-large"></i>
+            <i v-if="is_loading" class="el-icon-loading" style="font-size: xx-large"></i>
+        </div>
         <div class="main-content">
-          <el-tabs v-if="!is_loading" class="content-tab" v-model="active_tab_ame" @tab-click="handle_tab_click"
+          <el-tabs class="content-tab" v-model="active_tab_ame" @tab-click="handle_tab_click"
           tab-position="top">
             <el-tab-pane v-if="recommend_search_toggle" label="推荐食谱" name="first">推荐食谱</el-tab-pane>
             <el-tab-pane v-if="!recommend_search_toggle" label="综合最佳" name="first">综合最佳</el-tab-pane>
@@ -57,17 +62,18 @@
             <el-tab-pane v-if="!recommend_search_toggle" label="做过最多" name="third">做过最多</el-tab-pane>
           </el-tabs>
           <div v-if="is_loading" class="content-loading-icon"></div>
-         <grid-layout v-if="!is_loading"
+         <grid-layout v-if="!is_loading" ref="RecipeGrid"
             id="recipe-grid"
             :layout.sync="layout"
             :col-num="12"
-            :is-draggable="false"
-            :is-resizable="false"
-            :is-mirrored="false"
             :vertical-compact="true"
-            :use-css-transforms="false"
             :preventCollision="true"
-            :responsive="true"
+            :isDraggable="false"
+            :isResizable="false"
+            :use-css-transforms="false"
+            :responsive="false"
+            :margin="[10, 10]"
+            :autoSize="true"
           >
             <grid-item v-for="item in layout" :key="item.i"
               :x.sync="item.x" :y.sync="item.y" :w.sync="item.w" :h.sync="item.h" :i="item.i"
@@ -75,9 +81,9 @@
               <el-container class="recipe-item">
                 <el-main>
                   <div class="effect-box">
-                    <el-image class="recipe-cover" :src="item.info.cover_img"></el-image>
+                    <el-image @load="on_cover_load($event,item.i)" class="recipe-cover" :src="item.info.cover_img"></el-image>
                     <div class="border-line2">
-                        <p>{{item.info.ingredient_str}}</p>
+                      <p class="recipe-ingredients">{{item.info.ingredient_str}}</p>
                     </div>
                   </div>
                 </el-main>
@@ -127,6 +133,8 @@ export default {
       recommend_search_toggle: true
     }
   },
+  created: function () {
+  },
   mounted () {
     window.addEventListener('scroll', () => {
       this.scroll_top = document.documentElement.scrollTop ||
@@ -170,7 +178,7 @@ export default {
       // 返回顶部动画特效
       setTimeout(function animation () {
         setTimeout(() => {
-          let targetY = 680
+          let targetY = 600
           if ($this.scroll_top < targetY) {
             let speed = (targetY - $this.scroll_top) / 7 // 步进速度
             $this.scroll_top += (speed < 2.8 ? 2.8 : speed)
@@ -196,6 +204,11 @@ export default {
         this.suggest_items = res.body.suggestions
       })
     },
+    on_cover_load: function (event, i) {
+      let newContainerHeight = event.target.parentElement.parentElement.parentElement.parentElement.scrollHeight
+      this.layout[i].h = newContainerHeight / 150
+      this.$refs.RecipeGrid.layoutUpdate()
+    },
     do_search: function () {
       this.is_loading = true
       this.recipeLayout = []
@@ -213,7 +226,7 @@ export default {
               ingredientStr += ', '
             }
             ingredientStr += hit.ingredients[j].title
-            if (ingredientStr.length > 32) {
+            if (ingredientStr.length > 30) {
               ingredientStr += '...'
               break
             }
@@ -223,10 +236,10 @@ export default {
         let index
         for (index in hits) {
           let len = recipeLayout.length
-          console.log('x', 2 * (len % 4), 'y', 2 * (len / 4))
+          // TODO find shortest column to insert
           recipeLayout.push({
             'x': 2 * (len % 4) + 0.4 * (len % 4),
-            'y': 2 * parseInt(len / 4),
+            'y': 6 * parseInt(len / 4),
             'w': 2,
             'h': 2,
             'i': recipeLayout.length,
@@ -236,7 +249,6 @@ export default {
         this.recommend_search_toggle = false
         this.layout = recipeLayout
         this.is_loading = false
-        // console.log(this.layout)
       })
     },
     clear_input: function () {
@@ -273,6 +285,7 @@ export default {
       this.suggestion_show_flag = true
     },
     do_recommendation: function () {
+      this.is_loading = true
       this.recommend_search_toggle = true
     }
   }
@@ -285,9 +298,10 @@ export default {
 
 .background-img{
   width: 100%;
-  height: 730px;
   position: relative;
   background: url('../assets/bg3.jpg') top no-repeat;
+  padding-bottom: 42%;
+  background-size: 100%;
 }
 
 .background-img:after{
@@ -299,8 +313,13 @@ export default {
   content: "";
   width: 100%;
   height: 100%;
-  height: 730px;
   z-index: 1;
+}
+
+.on-background-content{
+  position: absolute;
+  left: 0; right:0;
+  height:100%;
 }
 
 .top-header{
@@ -313,9 +332,15 @@ export default {
   background: #fafafa;
 }
 
-.header-menu{
+.header-menu-group{
   position: fixed;
   margin-top: 22px;
+  left: 18%;
+}
+
+.header-menu{
+  position: relative;
+  display: inline-block;
   font-size: large;
   cursor: pointer;
   -webkit-user-select: none;
@@ -325,11 +350,11 @@ export default {
 }
 
 #find-menu{
-  left: 21%;
+
 }
 
 #search-menu{
-  left: 27%;
+  margin-left:25px;
 }
 
 .top-header .search-input{
@@ -337,21 +362,21 @@ export default {
 }
 
 .float-search .search-input {
-  height: 45px;
-  width: 600px;
+  width: 40%;
+  min-width: 350px;
   left: 0;
   right: 0;
   margin: 0 auto;
-  margin-top: 120px;
-  position: absolute;
+  margin-top: 8%;
+  position: relative;
   display: flex;
   z-index: 999;
 }
 
 .fixed-search .search-input{
   position: fixed;
-  height: 45px;
-  width: 600px;
+  width: 40%;
+  min-width: 250px;
   left: 0;
   right: 0;
   margin: 0 auto;
@@ -361,7 +386,7 @@ export default {
 }
 
 .fixed-search .search-input input{
-  width: 800px;
+  width: 100%;
   background: rgb(253, 253, 253);
   border: 2px solid #e2e1e1;
   box-shadow: 0 4px 10px rgba(0,0,0,0.12), 0 0 1px rgba(0,0,0,0.05) inset;
@@ -373,7 +398,7 @@ export default {
   border-radius: 39px;
   box-sizing: border-box;
   box-shadow: 0 4px 10px rgba(0,0,0,0.5), 0 0 2px rgba(0,0,0,0.05) inset;
-  width: 500px;
+  width: 84%;
   height: 45px;
   outline:none;
   font-size: 18px;
@@ -474,19 +499,28 @@ export default {
 
 .search-description{
   position: absolute;
-  left:0; right:0;
-  margin-left: -120px;
+  display: inline-block;
+  width: 100%;
   color: white;
-  margin-top:180px;
+  margin-top:58px;
   z-index: 10;
+}
+
+#search-description-left{
+  float: left;
+  padding-left: 5%;
+}
+
+#search-description-right{
+  float: right;
+  padding-right: 22%;
 }
 
 .navigator{
   position: absolute;
-  left:0; right:0;
-  margin-left: -120px;
+  left: 0; right: 0;
   color: white;
-  margin-top:600px;
+  margin-top: -120px;
   z-index: 10;
   -webkit-user-select: none;
   -moz-user-select: none;
@@ -518,14 +552,14 @@ export default {
 
 .content-tab{
   font-size: 0px;
-  margin-left: 100px;
-  margin-top: 40px;
+  margin-left: 6%;
+  margin-top: 30px;
   width: 360px;
 }
 
 .content-loading-icon{
   width: 300px;
-  height: 300px;
+  height: 350px;
   display:inline-block;
   vertical-align: middle;
   background: url('../assets/loading.gif') top no-repeat;
@@ -539,7 +573,7 @@ export default {
 }
 
 .recipe-item{
-  width: 300px;
+  width: 100%;
   background-color: white;
   border-radius: 6px;
   box-shadow: 0 2px 6px rgba(10, 10, 10, 0.15);
@@ -569,9 +603,13 @@ export default {
 .recipe-cover{
   position: relative;
   display: block;
-  width: 280px; height: 180px;
+  width: 100%; height: 100%;
   border-radius: 5px;
   cursor:pointer;
+}
+
+.recipe-ingredients{
+  position:relative;
 }
 
 .recipe-title{
@@ -654,6 +692,8 @@ export default {
 
 .el-main{
   padding:10px;
+  overflow-x: hidden;
+  overflow-y: hidden;
 }
 
 .el-header{
@@ -715,6 +755,9 @@ export default {
 
 .effect-box .border-line2 {
   position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   top: 0;
   left: 0;
   width: 100%;
@@ -726,6 +769,7 @@ export default {
   text-transform: uppercase;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
+
 }
 
 .effect-box .border-line2::after,
